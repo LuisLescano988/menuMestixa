50a9003c33547f587b5a75b3cd9b34aaec10c0d0
Luis Lescano <luislescanologo@hotmail.com>
2023-11-22 14:25:50 -0300

    fixeds at 90% menus

--- src/views/English.jsx
+++ src/views/English.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { MobilePDFReader } from 'react-read-pdf';
+// import { MobilePDFReader } from 'react-read-pdf';
+import { Viewer, Worker, SpecialZoomLevel } from '@react-pdf-viewer/core'
 import pdf from '../assets/englishMenu.pdf'
 
 
 function English() {
   return (
     <div className=' bg-black'>
-      <MobilePDFReader url={pdf}
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl={pdf}
+          defaultScale={SpecialZoomLevel.PageFit}
+        />
+      </Worker>
+      {/* <MobilePDFReader url={pdf}
         scale={auto}   
         isShowHeader={false}
         isShowFooter={false}
-      />
+      /> */}
     </div>
   )
 }
