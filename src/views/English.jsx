219500b132859eceaed6a7c0ad62bb3d9ce5407d
Luis Lescano <luislescanologo@hotmail.com>
2023-11-23 13:53:07 -0300

    menus fixed... ?

--- src/views/English.jsx
+++ src/views/English.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 // import { Viewer, Worker, SpecialZoomLevel } from '@react-pdf-viewer/core'
-import { Document, Page } from 'react-pdf';
-import pdf from '../assets/englishMenu.pdf'
+// import { Document, Page } from 'react-pdf';
+// import pdf from '../assets/englishMenu.pdf'
+import first from '../assets/eng1.jpg';
+import second from '../assets/eng2.jpg';
 
 
 function English() {
   return (
     <div className=' bg-black'>
-       <Document file={pdf}>
-        <Page pageNumber={1} />
-      </Document>
+      <img src={first} alt="" />
+      <img src={second} alt="" />
       {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
         <Viewer
           fileUrl={pdf}
