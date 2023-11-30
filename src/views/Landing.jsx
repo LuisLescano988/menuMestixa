aa82341586ae687f398d98d9f6e5404d2ad7503e
Luis Lescano <luislescanologo@hotmail.com>
2023-11-30 13:40:01 -0300

    menus languages fixed

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import header from '../assets/header.jpg'
+import header2 from '../assets/header2.jpg'
 import espanol from '../assets/espanol.jpg'
 import ingles from '../assets/ingles.jpg'
 import footer from '../assets/footer.jpg'
+import footer2 from '../assets/footer2.jpg'
 import { NavLink } from 'react-router-dom'
+// import full from '../assets/mestixaFull.jpg'
 
 const Home = () => {
   return (
-    <div className=' bg-white overflow-hidden bottom-0'>
+    <div className=' bg-white h-fit overflow-hidden'>
       <img src={header} />
+      <img src={header2} />
       <div className=' flex flex-col '>
         <button className=''>
           <NavLink to={'/spanish'}>
@@ -21,7 +25,8 @@ const Home = () => {
           </NavLink>
         </button>
       </div>
-      <img src={footer} className=' md:relative absolute bottom-0 -my-20 p-0' />
+      <img src={footer} className=' bottom-0' />
+      <img src={footer2} className=' bottom-0' />
     </div>
   )
 }
