43ace5c684cd7936c21c2f0c731316b0bc1c54f3
Luis Lescano <luislescanologo@hotmail.com>
2024-10-02 19:16:16 -0300

    fix responsive 2

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -10,17 +10,17 @@ import { NavLink } from 'react-router-dom'
 const Home = () => {
   return (
     <div className=' flex flex-col items-center justify-end bg-black h-screen overflow-hidden'>
-      <img src={header} className=' md:w-1/3 -mb-[0.2%]' />
-      <img src={header2} className=' md:w-1/3' />
-      <div className=' flex flex-col md:w-1/3'>
+      <img src={header} className=' lg:w-1/3 -mb-[0.2%]' />
+      <img src={header2} className=' lg:w-1/3' />
+      <div className=' flex flex-col lg:w-1/3'>
         <button className=' -mb-[0.5%]'>
           <NavLink to={'/spanish'}>
-            <img src={espanol} className=' md:h-20 md:w-full md:object-contain' />
+            <img src={espanol} className=' lg:h-20 lg:w-full lg:object-contain' />
           </NavLink>
         </button>
         <button>
           <NavLink to={'/english'}>
-            <img src={ingles} className=' md:h-20 md:w-full md:object-contain'/>
+            <img src={ingles} className=' lg:h-20 lg:w-full lg:object-contain'/>
           </NavLink>
         </button>
         <button className=''>
@@ -29,7 +29,7 @@ const Home = () => {
           </NavLink>
         </button>
       </div>
-      <img src={footer2} className=' md:w-1/3 bottom-0' />
+      <img src={footer2} className=' lg:w-1/3 bottom-0' />
     </div>
   )
 }
