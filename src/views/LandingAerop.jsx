42a513b7a497297089f7763760b41449c290ac50
LuisLescano988 <luislescanologo@hotmail.com>
2024-11-14 13:17:15 -0300

    fix responsive 1

--- src/views/LandingAerop.jsx
+++ src/views/LandingAerop.jsx
@@ -6,13 +6,13 @@ import english from '../assets/newIngles.jpg'
 const LandingAirport = () => {
   return (
     <div className=' flex justify-center relative'>
-        <img className=' absolute h-screen' src={landingAirport} alt="" />
-        <div className=' flex flex-col items-center justify-center h-screen '>
+        <img className=' absolute max-sm:bottom-0 h-screen max-sm:h-5/6 max-sm:w-full max-sm:object-cover' src={landingAirport} alt="" />
+        <div className=' flex flex-col items-center max-sm:pt-[23%] justify-center h-screen '>
             <a className='flex flex-col items-center z-10' href="/spanishport">
-                <img className=' cursor-pointer w-1/3' src={spanish} alt="" />
+                <img className=' cursor-pointer max-sm:w-full w-1/3' src={spanish} alt="" />
             </a>
             <a className='flex flex-col items-center z-10' href="/englishport">
-                <img className=' cursor-pointer w-1/3' src={english} alt="" />
+                <img className=' cursor-pointer max-sm:w-full w-1/3' src={english} alt="" />
             </a>
         </div>
     </div>
