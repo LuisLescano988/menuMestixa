cc09213241984c515aa025610d074cd39821947a
LuisLescano988 <luislescanologo@hotmail.com>
2024-11-15 15:48:27 -0300

    que sea un poco lo q dios quiera me canse hoy con los pdfs

--- src/views/EnglishAirport.jsx
+++ src/views/EnglishAirport.jsx
@@ -1,10 +1,12 @@
-import first from '../assets/menuEngAirp.png';
+// import first from '../assets/menuEngAirp.png';
+import menu from '../assets/menuEng.pdf'
 
 
 function EnglishAirport() {
   return (
     <div className=' bg-black'>
-      <img className=' w-screen' src={first} alt="" />
+      {/* <img className=' w-screen' src={first} alt="" /> */}
+      <object className='' data={menu} type="application/pdf"></object>
     </div>
   )
 }
--- src/views/SpanishAirport.jsx
+++ src/views/SpanishAirport.jsx
@@ -1,10 +1,12 @@
-import first from '../assets/menuSpaAir.png';
+// import first from '../assets/menuSpaAir.png';
+import menu from '../assets/menuEsp.pdf'
 
 function SpanishAirport() {
 
     return (
         <div className=' bg-black w-screen'>
-            <img className=' w-screen' src={first} alt="" />
+            {/* <img className=' w-screen' src={first} alt="" /> */}
+            <object className='' data={menu} type="application/pdf"></object>
         </div>
     )
 }
