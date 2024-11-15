12199c446ab91d46b9b73b70104b24cac520da8a
LuisLescano988 <luislescanologo@hotmail.com>
2024-11-15 12:00:21 -0300

    fix responsive 3

--- src/views/LandingAerop.jsx
+++ src/views/LandingAerop.jsx
@@ -7,23 +7,6 @@ import english from '../assets/newIngles.jpg'
 
 
 const LandingAirport = () => {
-//   return (
-//     <div className=' flex justify-center relative'>
-//         <img className=' absolute max-sm:bottom-0 h-screen max-sm:h-[88%] max-sm:w-full max-sm:object-cover' src={landingAirport} alt="" />
-//         <div className=' flex flex-col items-center max-sm:pt-[23%] justify-center h-screen '>
-//             <a className='flex flex-col items-center z-10' href="/spanishport">
-//                 <img className=' cursor-pointer max-sm:w-full w-1/3' src={spanish} alt="" />
-//             </a>
-//             <a className='flex flex-col items-center z-10' href="/englishport">
-//                 <img className=' cursor-pointer max-sm:w-full w-1/3' src={english} alt="" />
-//             </a>
-//         </div>
-//     </div>
-//   )
-// }
-
-
-  // Componente móvil
   const MobileView = () => (
     <div className=' flex justify-center relative'>
     <img className=' absolute max-sm:top-0 h-screen max-sm:h-[45%] max-sm:w-full max-sm:object-cover' src={header} alt="" />
